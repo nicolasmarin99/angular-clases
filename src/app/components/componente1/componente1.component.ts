1d41cf7f3c9ca5ab284494fce280706a4da8911b
unknown <SSDD@PNAWSA05LC1116.duoc.acad>
2024-08-28 09:47:50 -0400

    pasar info entre componentes y componentes

--- src/app/components/componente1/componente1.component.ts
+++ src/app/components/componente1/componente1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-componente1',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente1.component.scss'],
 })
 export class Componente1Component  implements OnInit {
+  @Input() titulo:string="";
+  apellido:string="";
 
   constructor() { }
 
